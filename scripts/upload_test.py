f468351a0551e298ec715f0059189ef34cf9035c
Hayato Arai <arai-hayato673@g.ecc.u-tokyo.ac.jp>
2022-06-18 19:04:11 +0900

    test github actions

--- scripts/upload_test.py
+++ scripts/upload_test.py
@@ -5,10 +5,7 @@ import shlex
 import subprocess
 from typing import List, Optional
 
-from google.auth.transport.requests import Request
 from google.oauth2 import service_account
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
